99f10d5cb0e244c5854d9a04dcb3a587beb81913
Chris Schmich <schmch@gmail.com>
2014-03-08 16:38:06 -0800

    Adding 'astro run' command to run processes with http_proxy environment variable.

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 var parser = require('nomnom');
 var astro = require('./common/control');
+var spawn = require('child_process').spawn;
 
 parser
   .command('start')
@@ -15,6 +16,25 @@ parser
   .callback(function() {
     var control = new astro.AstroControl();
     control.stop();
+  });
+
+parser
+  .command('run')
+  .callback(function(opts) {
+
+    // TODO: Check for empty.
+    var args = opts._.splice(1);
+    var command = args.shift();
+
+    // TODO: Get URL/port from astro settings.
+    var env = process.env;
+    env.http_proxy = 'http://localhost:8080';
+
+    // TODO: Handle errors (command not found, etc.).
+    var child = spawn(command, args, { stdio: 'inherit', env: env });
+    child.on('close', function(code) {
+      process.exit(code);
+    });
   });
 
 parser.parse();
